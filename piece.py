49e8875bfb59d3250e5854767590ffb6b3bcecd1
Sekai1 <timothy.hylau@gmail.com>
2021-10-04 22:33:31 +0100

    stuff

---
+++ piece.py
@@ -0,0 +1,95 @@
+#piece class
+
+#diagonal sliding for queens and bishops
+def diagonal_check(location1, location2):
+	if (location1 % 17 == location2 % 17):
+		return 1
+
+	elif (location1 % 15 == location2 % 15):
+		return -1
+
+#diagonal sliding for queens and rooks
+def straight_check(location1, location2):
+	if (board_file(location1) == board_file(location2)):
+		if (board_rank(location1) == board_rank(location2)):
+			return True
+
+class Piece():
+	def __init__(self, colour, location):
+		self.colour = colour
+		self.location = location
+
+class Pawn(Piece):
+	def __init__(self, colour, location):
+		Piece.__init__(self, colour, location)
+		self.value = 1
+		self.character = 'P'
+		if self.colour == 'white':
+			self.character = self.character.lower()
+
+	def movePiece(self, destination):
+		pass
+
+class Knight(Piece):
+	def __init__(self, colour, location):
+		Piece.__init__(self, colour, location)
+		self.value = 3
+		self.character = 'N'
+		if self.colour == 'white':
+			self.character = self.character.lower()
+
+	def movePiece(self, destination):
+		if (board_rank(destination) == board_rank(self.location) + 2) or (board_rank(destination) == board_rank(self.location) - 2):
+			if (board_file(destination) == board_file(self.location) + 1) or (board_file(destination) == board_file(self.location) - 1):
+				return True
+
+		elif (board_rank(destination) == board_rank(self.location) + 1) or (board_rank(destination) == board_rank(self.location) - 1):
+			if (board_file(destination) == board_file(self.location) + 2) or (board_file(destination) == board_file(self.location) - 2):
+				return True
+
+		else:
+			return False
+
+class Bishop(Piece):
+	def __init__(self, colour, location):
+		Piece.__init__(self, colour, location)
+		self.value = 3
+		self.character = 'B'
+		if self.colour == 'white':
+			self.character = self.character.lower()
+
+	def movePiece(self, destination):
+		pass
+
+class Rook(Piece):
+	def __init__(self, colour, location ):
+		Piece.__init__(self, colour, location)
+		self.value = 5
+		self.character = 'R'
+		if self.colour == 'white':
+			self.character = self.character.lower()
+
+	def movePiece(self, destination):
+		pass
+
+class Queen(Piece):
+	def __init__(self, colour, location):
+		Piece.__init__(self, colour, location)
+		self.value = 9
+		self.character = 'Q'
+		if self.colour == 'white':
+			self.character = self.character.lower()
+
+	def movePiece(self, destination):
+		pass
+
+class King(Piece):
+	def __init__(self, colour, location):
+		Piece.__init__(self, colour, location)
+		self.value = 10000000
+		self.character = 'K'
+		if self.colour == 'white':
+			self.character = self.character.lower()
+
+	def movePiece(self, destination):
+		pass
---
+++ pychess.py
@@ -0,0 +1,113 @@
+from misc import *
+from piece import *
+
+#Board Class
+class ChessBoard:
+	def __init__(self):
+		self.board = []
+		for i in range(128):
+			self.board.append(None)
+
+		#adding pieces
+		#add white pieces
+		self.board[0] = Rook('white', 0)
+		self.board[1] = Knight('white', 1)
+		self.board[2] = Bishop('white', 2)
+		self.board[3] = Queen('white', 3)
+		self.board[4] = King('white', 4)
+		self.board[5] = Bishop('white', 5)
+		self.board[6] = Knight('white', 6)
+		self.board[7] = Rook('white', 7)
+
+		#add black pieces
+		self.board[112] = Rook('black', 112)
+		self.board[113] = Knight('black', 113)
+		self.board[114] = Bishop('black', 114)
+		self.board[115] = Queen('black', 115)
+		self.board[116] = King('black', 116)
+		self.board[117] = Bishop('black', 117)
+		self.board[118] = Knight('black', 118)
+		self.board[119] = Rook('black', 119)
+
+		#add all pawns
+		for i in range (128):
+			if 16 <= i <= 23:
+				self.board[i] = Pawn('white', i)
+
+			elif 96 <= i <= 103:
+				self.board[i] = Pawn('black', i)
+
+	def move(self, location1, location2):
+		if self.__selfTake(location1, location2) == True:
+			if self.__offBoardCheck(location2) == True:
+				print('pass')
+
+	def __selfTake(self, location1, location2):
+		if self.board[location1] != None:
+			colour1 = self.board[location1].colour
+
+		else:
+			colour1 = None
+
+		if self.board[location2] != None:
+			colour2 = self.board[location2].colour
+
+		else:
+			colour2 = None
+
+		if colour1 == None and colour2 == None:
+			return True
+
+		elif colour1 == colour2:
+			return False
+
+		else:
+			return True
+
+	def __offBoardCheck(self, hexsquare):
+		if (hexsquare & 0x88) == 0:
+			return True
+
+		else:
+			return False
+
+	def print_board(self):
+		print('board:')
+		x = 0
+		for i in range(8):
+			out = []
+			for j in range(16):
+				try:
+					out.append(self.board[x].character)
+				except:
+					out.append('o')
+				x += 1
+			print(out)
+
+#player class
+class player():
+	def __init__(self):
+		pass
+
+#chess ai class
+class computer(player):
+	def __init__(self):
+		player.__init__(self)
+		pass
+
+#Elo Calculation
+def expectedScore(Ra, Rb):
+	Ea = 1 / (1 + 10 ** ((Rb - Ra) / 400))
+	return Ea
+
+def updateElo(Ra, Sa, Ea):
+	NewRa = Ra + 34 *(Sa - Ea)
+	return NewRa
+
+def game():
+	board = ChessBoard()
+	board.print_board()
+	board.move(0x0, 0x20)
+
+if __name__ == '__main__':
+	game()
