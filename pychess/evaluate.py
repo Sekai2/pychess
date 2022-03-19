27e3fa3c6d7e79525cbf015c7beefc7175e0daec
Sekai1 <timothy.hylau@gmail.com>
2022-03-19 12:40:28 +0000

    transfer

--- pychess/evaluate.py
+++ pychess/evaluate.py
@@ -1,10 +1,27 @@
-#Evalutation code
-class evaluate():
-	def material(board):
+import numba
+from numba import jit
+
+def countMaterial(board):
 		count = []
 		pieceChar = ['PNBRQKpnbrqk']
 		for i in range(12):
 			count.append(0)
 
 		for i in board:
-			count[pieceChar.index(i.character)] += 1
+			if i != None:
+				count[pieceChar.index(i.character)] += 1
+
+		return count
+
+#Evalutation code
+class evaluate():
+	@jit(nopython = True)
+	def valMult(a, b):#for material calculation
+		return a * b
+
+	@jit(nopython = True)
+	def valSub(a, b):
+		return a - b
+
+	def material(count):
+		return reduce()
